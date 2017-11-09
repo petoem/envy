c48fdf8d1aad027e3a8686590ae1084c7b709434
Michael Petö <michael@petoe.me>
2017-11-09 14:50:20 +0100

    Add Envy #load. #load! and #parse methods

--- src/envy.cr
+++ src/envy.cr
@@ -1,5 +1,39 @@
 require "./envy/*"
 
 module Envy
-  # TODO Put your code here
+  extend self
+  DEFAULT_ENV_FILENAME = ".env"
+
+  {% for name, comp in {load: :"||=", load!: :"="} %}
+  def {{name.id}}(*filename : String)
+    filename.each do |file|
+      env_vars = parse file
+      {{name.id}} env_vars
+    end
+    {{name.id}} parse(DEFAULT_ENV_FILENAME) if filename.size.zero?
+  end
+
+  def {{name.id}}(hash : Hash(String, String))
+    hash.each do |key, value|
+      ENV[key] {{comp.id}} value
+    end
+  end
+  {% end %}
+
+  def parse(filename = String) : Hash(String, String)
+    env_vars = {} of String => String
+    File.each_line File.expand_path(filename) do |line|
+      line = line.strip
+      next if line.blank?
+      next if line.starts_with? '#'
+      next unless line.includes? '='
+      line = line.lchop("export").strip if line.starts_with? "export"
+      key, value = line.split '=', 2
+      env_vars[key.upcase] = value.strip '"'
+    end
+    env_vars
+  rescue
+    puts "ENVY - Failed to load #{filename}"
+    {} of String => String
+  end
 end
