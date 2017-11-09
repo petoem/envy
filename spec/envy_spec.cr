a8b1e54dbb1bdef131ed448471167f2789c9d46b
Michael Petö <michael@petoe.me>
2017-11-09 15:24:31 +0100

    Add spec

--- spec/envy_spec.cr
+++ spec/envy_spec.cr
@@ -1,9 +1,45 @@
 require "./spec_helper"
 
 describe Envy do
-  # TODO: Write tests
+  it "load hash" do
+    ENV["ENVY_TEST"] = "1234"
+    Envy.load({"ENVY_TEST" => "something else", "APP_DB" => "myawesomedb"})
+    ENV["ENVY_TEST"].should eq("1234")
+    ENV["APP_DB"].should eq("myawesomedb")
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "load! hash" do
+    ENV["ENVY_TEST"] = "1234"
+    Envy.load!({"ENVY_TEST" => "something else", "APP_DB" => "myawesomedb"})
+    ENV["ENVY_TEST"].should eq("something else")
+    ENV["APP_DB"].should eq("myawesomedb")
+  end
+
+  it "load .env file" do
+    ENV["A_KEY"] = "one value"
+    Envy.load "spec/.env"
+    ENV["A_KEY"].should eq("one value")
+    ENV["ANOTHER_LINE"].should eq("some_value")
+  end
+
+  it "load! .env file" do
+    ENV["A_KEY"] = "one value"
+    Envy.load! "spec/.env"
+    ENV["A_KEY"].should eq("value")
+    ENV["ANOTHER_LINE"].should eq("some_value")
+  end
+
+  it "parse" do
+    env_hash = {"CRYSTAL_ENV"          => "development",
+                "A_KEY"                => "value",
+                "ANOTHER_LINE"         => "some_value",
+                "EMPTY_VALUE"          => "",
+                "REMOVE_DOUBLE_QUOTES" => "double",
+                "EQUAL_SIGNS"          => "sign==one=",
+                "RETAIN_INNER_QUOTES"  => "{ \"foo\": \"bar\" }",
+                "SPACE_SHIP"           => "some spaced out ship",
+                "UPCASE_KEY"           => "https://db/path",
+                "EXPORT_THIS_MAIL"     => "hello@world.tld"}
+    Envy.parse("spec/.env").should eq(env_hash)
   end
 end
