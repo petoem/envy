ba7a6f2a1f8cdd77579270b0a89ba8101e5b8640
Youssef Idmoussi <youssef.idmoussi@gmail.com>
2018-02-11 02:00:26 +0000

    Add with expetion when .env file does not exist:
    - Add Envy#load and Envy#load! with block, that takes a hash of
    configuration '{ raise_exception: true }'.

--- src/envy.cr
+++ src/envy.cr
@@ -4,13 +4,23 @@ module Envy
   extend self
   DEFAULT_ENV_FILENAME = ".env"
 
+  class InvalidFileException < Exception; end
+
   {% for name, comp in {load: :"||=", load!: :"="} %}
   def {{name.id}}(*filename)
     filename.each do |file|
-      env_vars = parse file
+      env_vars = parse(file)
       {{name.id}} env_vars
     end
     {{name.id}} parse(DEFAULT_ENV_FILENAME) if filename.size.zero?
+  end
+
+  def {{name.id}}(*filename, &block)
+    filename.each do |file|
+      env_vars = parse(file, yield)
+      {{name.id}} env_vars
+    end
+    {{name.id}} parse(DEFAULT_ENV_FILENAME, yield) if filename.size.zero?
   end
 
   def {{name.id}}(hash : Hash(String, String))
@@ -20,7 +30,7 @@ module Envy
   end
   {% end %}
 
-  def parse(filename : String) : Hash(String, String)
+  def parse(filename : String, hash = {} of Symbol => Bool) : Hash(String, String)
     env_vars = {} of String => String
     File.each_line File.expand_path(filename) do |line|
       line = line.strip
@@ -33,6 +43,7 @@ module Envy
     end
     env_vars
   rescue
+    raise InvalidFileException.new("ENVY - Failed to load #{filename}") if hash[:raise_exception]?
     puts "ENVY - Failed to load #{filename}"
     {} of String => String
   end
